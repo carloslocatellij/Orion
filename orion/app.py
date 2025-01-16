2a0d1ccfe7c6d3a063e0aa1f30b4f4a577f99964
carloslocatellijr <call.jmail@gmail.com>
2025-01-15 21:28:48 -0300

    Setando pydantic

--- orion/app.py
+++ orion/app.py
@@ -1,8 +1,10 @@
+from http import HTTPStatus
 from fastapi import FastAPI
+from orion.schemas import *
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá mundo do FastApi! \n I Love You!'}
