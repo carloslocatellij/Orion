39ed58dacc453395d77646c26b4d95c2cc0bd674
Carlos A. L. Jr. <61094411+carloslocatellij@users.noreply.github.com>
2025-02-03 19:29:47 +0000

    implementação de endpoints para usuários

--- orion/app.py
+++ orion/app.py
@@ -1,10 +1,27 @@
 from http import HTTPStatus
+
 from fastapi import FastAPI
-from orion.schemas import *
+
+from orion.schemas import Message, User, UserDB, UserList, UserPublic
 
 app = FastAPI()
+
+database = []
 
 
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return {'message': 'Olá mundo do FastApi! \n I Love You!'}
+    return {'message': 'Olá mundo do FastApi! I Love You!'}
+
+
+@app.post('/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic)
+def create_user(user: User):
+    user_id = UserDB(**user.model_dump(), id=len(database) + 1)
+    database.append(user_id)
+
+    return user_id
+
+
+@app.get('/users/', response_model=UserList)
+def read_users():
+    return {'users': database}
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,15 +1,35 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from orion.app import app
-
-
-def test_root_deveser_ok_e_ola_mundo():
-    client = TestClient(app)
+def test_root_deveser_ok_e_ola_mundo(client):
     response = client.get('/')
 
     assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'Olá mundo do FastApi! I Love You!'}
+
+
+def test_create_user_deve_criar_usuario_e_return_201(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'teste',
+            'email': 'test@exemplo.com',
+            'password': '123456',
+        },
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
     assert response.json() == {
-        'message': 'Olá mundo do FastApi! \n I Love You!'
+        'username': 'teste',
+        'email': 'test@exemplo.com',
+        'id': 1,
+    }
+
+
+def test_read_users_deve_retornar_lista_de_users(client):
+    response = client.get('/users/')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [{'username': 'teste', 'email': 'test@exemplo.com', 'id': 1}]
     }
