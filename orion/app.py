75e0d261d35e2951c7a32fa96f36db1a1b74c930
Carlos A. L. Jr. <61094411+carloslocatellij@users.noreply.github.com>
2025-02-05 20:03:19 +0000

    endpoint update_user

--- orion/app.py
+++ orion/app.py
@@ -25,3 +25,11 @@ def create_user(user: User):
 @app.get('/users/', response_model=UserList)
 def read_users():
     return {'users': database}
+
+@app.put('/users/{user_id}', response_model=UserPublic)
+def update_user(user_id: int, user: User):
+    if user_id > len(database) or user_id < 1:
+        raise HTTPException(status_code-HTTPStatus.NOT_FOUND, detail='User not found')
+    user_id = UserDB(**user.model_dump(), id=user_id)
+
+    return user_id
